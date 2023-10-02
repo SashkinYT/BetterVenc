33a91eacab23f389aba4a274eb2c93715b79038f
It's Sashkin <74656533+SashkinYT@users.noreply.github.com>
2023-10-02 23:44:07 +0700

    Create CallMusicChanger.js

---
+++ Plugins/CallMusicChanger.js
@@ -0,0 +1,16 @@
+addPluginToList("CallMusicChanger","https://discord.com/assets/b9411af07f154a6fef543e7e442e4da9.mp3")
+var CSCBVCcallmusic = getBVCPluginSettings("CallMusicChanger")
+function changeCallMusic(music){
+    var play = Audio.prototype.play;
+    Audio.prototype.play = function() {
+    this.src = this.src.replace('https://discord.com/assets/84a1b4e11d634dbfa1e5dd97a96de3ad.mp3', music)
+    return play.apply(this, arguments)
+}
+}
+changeCallMusic(CSCBVCcallmusic)
+setInterval(()=>{
+    if(CSCBVCcallmusic!=getBVCPluginSettings("CallMusicChanger")){
+        CSCBVCcallmusic = getBVCPluginSettings("CallMusicChanger")
+        changeCallMusic(CSCBVCcallmusic)
+    }
+}, 1000)
