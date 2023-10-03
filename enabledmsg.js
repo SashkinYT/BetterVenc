d5074f07e01bf3b1c63bfc4a6597108aa4a7c999
It's Sashkin <74656533+SashkinYT@users.noreply.github.com>
2023-10-03 12:30:37 +0700

    Update enabledmsg.js

--- enabledmsg.js
+++ enabledmsg.js
@@ -1,2 +1,4 @@
+//PluginName: BVCEnabledMessage
+//DefaultSettings: This plugin has no settings
 console.log("[BetterVenc] Enabled!")
 addPluginToList("BVCEnabledMessage","This plugin has no settings")
