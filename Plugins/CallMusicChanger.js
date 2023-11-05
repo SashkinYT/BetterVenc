8c26d5f05f35e22fe124b3f49e8fe5aeab67af60
It's Sashkin <74656533+SashkinYT@users.noreply.github.com>
2023-11-05 20:33:52 +0700

    Update CallMusicChanger.js

--- Plugins/CallMusicChanger.js
+++ Plugins/CallMusicChanger.js
@@ -1,6 +1,6 @@
 //PluginName: CallMusicChanger
 //DefaultSettings: https://discord.com/assets/b9411af07f154a6fef543e7e442e4da9.mp3
-addPluginToList("CallMusicChanger", "https://discord.com/assets/e72bd9a68c5a5de4b084.mp3")
+addPluginToList("CallMusicChanger", "https://discord.com/assets/b9411af07f154a6fef543e7e442e4da9.mp3")
 var CSCBVCcallmusic = getBVCPluginSettings("CallMusicChanger")
 function changeCallMusic(music){
     var play = Audio.prototype.play;
