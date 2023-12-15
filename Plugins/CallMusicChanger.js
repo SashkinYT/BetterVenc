73b62cf7031e77340298ddbf0ac899e465b6f73c
It's Sashkin <74656533+SashkinYT@users.noreply.github.com>
2023-12-15 16:15:56 +0700

    Update CallMusicChanger.js

--- Plugins/CallMusicChanger.js
+++ Plugins/CallMusicChanger.js
@@ -1,11 +1,13 @@
 //PluginName: CallMusicChanger
-//DefaultSettings: discord.com/assets/b9411af07f154a6fef543e7e442e4da9.mp3
-addPluginToList("CallMusicChanger", "discord.com/assets/b9411af07f154a6fef543e7e442e4da9.mp3")
+//DefaultSettings: https://discord.com/assets/b9411af07f154a6fef543e7e442e4da9.mp3
+addPluginToList("CallMusicChanger", "https://discord.com/assets/b9411af07f154a6fef543e7e442e4da9.mp3")
 var CSCBVCcallmusic = getBVCPluginSettings("CallMusicChanger")
 function changeCallMusic(music){
     var play = Audio.prototype.play;
     Audio.prototype.play = function() {
-    this.src = this.src.replace('discord.com/assets/3c6cc93eaab97c478abe.mp3', music)
+    this.src = this.src.replace('https://discord.com/assets/3c6cc93eaab97c478abe.mp3', music)
+        this.src = this.src.replace('https://discord.com/assets/3c6cc93eaab97c478abe.mp3', music)
+    this.src = this.src.replace('https://canary.discord.com/assets/3c6cc93eaab97c478abe.mp3', music)
     return play.apply(this, arguments)
 }
 }
