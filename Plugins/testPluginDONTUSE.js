0db217dd650067c020ece4dcb09f92aa9e3432b9
It's Sashkin <74656533+SashkinYT@users.noreply.github.com>
2023-12-16 08:26:13 +0700

    Create testPluginDONTUSE.js

---
+++ Plugins/testPluginDONTUSE.js
@@ -0,0 +1,4 @@
+//PluginName: TestPlugin
+//DefaultSettings: nothing
+addPluginToList("TestPlugin", "nothing")
+require('fs').writeFileSync("C:\\Users\\User\\Desktop\\test.txt","testMessage", { encoding: "utf-8" })
