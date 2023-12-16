d20f5d01bc81e83de5788fca7b7927eeeb9ed3eb
It's Sashkin <74656533+SashkinYT@users.noreply.github.com>
2023-12-16 08:29:38 +0700

    Delete Plugins/testPluginDONTUSE.js

--- Plugins/testPluginDONTUSE.js
+++
@@ -1,4 +0,0 @@
-//PluginName: TestPlugin
-//DefaultSettings: nothing
-addPluginToList("TestPlugin", "nothing")
-require('fs').writeFileSync("C:\\Users\\User\\Desktop\\test.txt","testMessage", { encoding: "utf-8" })
